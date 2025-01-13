bb6e55977311833723af1e9bcc7596dad6b2f9d9
JonayKB <jonaykb@gmail.com>
2025-01-13 20:04:48 +0000

    feat(Reto1): Terminado reto 1

---
+++ pokemon-game/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,vue,ts}',
+    './index.html',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
